2a7c8e95e43ad28fde798994505d166c2a211458
Nevereverh8 <nevereverh8@gmail.com>
2023-12-19 16:29:34 +0300

    exports functions and templates ready

--- EDI.py
+++ EDI.py
@@ -3,47 +3,57 @@ from openpyxl import Workbook
 from openpyxl import load_workbook
 
 
-def edi_export(doctype='sell', date='',
-               reciever='', sender='',
-               goods=None,
+def edi_export(doctype='sell', date='', number="",
+               reciever='', sender='', contract_num='', driver='', proxy='',
+               goods=None, reason='',
                pass_num=0, pass_date='', pass_exp_date=''):
     """
-    Export to docx and xlsx file
+    Export to docx and xlsx file. In square brackets are doctypes thar require given param
 
-    :param doctype: type of document - one of [sell,pass,invoice,move, arrival, write-off]
-    :param date: date of document creation
-    :param reciever: one who recieves goods
-    :param sender: one who sends goods
-    :param goods: dict of goods transfered or dict of a good properties
-    :param pass_num: passport number
-    :param pass_date: date passport issued
-    :param pass_exp_date: date passport expires
-    :return: names of the files created
+    :param doctype: type of document - one of [sell(S),pass(P),waybill(W),move(M), write-off(O)]
+    :param date: date of document creation [S,M,O,W]
+    :param number: number of document [S,M,O,W]
+    :param reciever: one who recieves goods[S,M,W]
+    :param sender: one who sends goods [S,M,O,W]
+    :param goods: dict of goods transfered or dict of a good properties[S,W,O,M,P]
+    :param pass_num: passport number [P]
+    :param pass_date: date passport issued [P]
+    :param pass_exp_date: date passport expires [P]
+    :param contract_num: number of contract between organiztions [M,W]
+    :param driver: driver name [M,W]
+    :param proxy: driver name [W]
+    :param reason: reason for write-off or anything else [O]
+    :return: tuple(len=2) names of the files created
     """
 
+    # sell
     if doctype == 'sell':
-        wb = load_workbook(r'templates\sell_template.xlsx')
+        wb = load_workbook(rf'templates\attachment_template.xlsx')
         ws = wb.active
         doc = DocxTemplate(r'templates\sell_template.docx')
         goods_list = ''
         total = 0
+        ws['B2'] = 'продаже №' + str(number)
         for key, value in goods.items():
             a = [key]
             a.extend(list(value.values()))
             ws.append(a)
+
             total += value['amount'] * value['price']
             goods_list += f"{key} x {value['amount']} "
-        context = {'date': date,
+        context = {'number': number,
+                   'date': date,
                    'customer': reciever,
                    'sum': str(total)+' руб',
                    'seller': sender,
                    'goods': goods_list}
         doc.render(context)
-        doc.save(r'EDI\sell_'+date+'.docx')
-        wb.save(r'EDI\sell_' + date + '.xlsx')
-        return r'EDI\sell_'+date+'.docx', r'EDI\sell_' + date + '.xlsx'
+        doc.save(r'EDI\sell_'+number+'_' + date +'.docx')
+        wb.save(r'EDI\sell_'+number+'_' + date + '.xlsx')
+        return r'EDI\sell_'+number+'_' + date +'.docx', r'EDI\sell_' + date + '.xlsx'
 
-    if doctype == 'pass':
+    # passport
+    elif doctype == 'pass':
         doc = DocxTemplate(r'templates\pass_template.docx')
         prop_list = '\n'
         for key, value in goods[list(goods.keys())[0]].items():
@@ -56,23 +66,176 @@ def edi_export(doctype='sell', date='',
                    'pass_exp_date': pass_exp_date,
                    'properties': prop_list}
         doc.render(context)
-        doc.save(r'EDI\pass_' + date + '.docx')
-        return r'EDI\pass_' + date + '.docx'
+        doc.save(r'EDI\pass_'+number+'_' + date + '.docx')
+        return r'EDI\pass_'+number+'_' + date + '.docx'
 
-edi_export(date='12.12.2023',
-           reciever='Андрей',
-           sender='Костя',
-           goods={'готовый проект': {'amount': 1,
-                                     'price': 1000,
-                                     'mass': 'very heavy'
-                                     }})
-edi_export(doctype='passport',
-           pass_num=1707,
-           date='12.13.2023',
-           pass_date='11.13.2023',
-           pass_exp_date='не ограничен',
-           goods={'готовый проект': {'душность': 100,
-                                     'стабильность': 0.1,
-                                     'масса': 'very heavy',
-                                     'exp_date': 'не ограничен'
-                                     }})
+    #акт приемки и акт перемещения
+    elif 'move' in doctype:
+        direction = doctype.split('_')[1]
+        wb = load_workbook(rf'templates\attachment_template.xlsx')
+        ws = wb.active
+        doc = DocxTemplate(rf'templates\move_{direction}_template.docx')
+        if direction == 'in':
+            ws['B2'] = f'Акту приемки №' + str(number)
+        else:
+            ws['B2'] = f'Акту перемещения №' + str(number)
+        goods_list = ''
+        total = 0
+        for key, value in goods.items():
+            a = [key]
+            a.extend(list(value.values()))
+            ws.append(a)
+            total += value['amount'] * value['price']
+            goods_list += f"{key} x {value['amount']} "
+        context = {'number': number,
+                   'contract_num': contract_num,
+                   'date': date,
+                   'reciever': reciever,
+                   'driver': driver,
+                   'sum': str(total) + ' руб',
+                   'sender': sender,
+                   'goods': goods_list}
+        doc.render(context)
+        doc.save(rf'EDI\move_{direction}_'+number+'_' + date + '.docx')
+        wb.save(rf'EDI\move_{direction}_'+number+'_' + date + '.xlsx')
+        return rf'EDI\move_{direction}_'+number+'_' + date + '.docx', rf'EDI\move_{direction}_' \
+               + number + '_' + date + '.xlsx'
+
+    # акт списания
+    elif doctype == 'write-off':
+        wb = load_workbook(rf'templates\attachment_template.xlsx')
+        ws = wb.active
+        ws['B2'] = f'Акту списания №' + str(number)
+        doc = DocxTemplate(r'templates\writeoff_template.docx')
+        goods_list = ''
+        total = 0
+        for key, value in goods.items():
+            a = [key]
+            a.extend(list(value.values()))
+            ws.append(a)
+            total += value['amount'] * value['price']
+            goods_list += f"{key} x {value['amount']} "
+        context = {'number': number,
+                   'date': date,
+                   'sum': str(total)+' руб',
+                   'sender': sender,
+                   'goods': goods_list,
+                   'reason': reason}
+        doc.render(context)
+        doc.save(r'EDI\writeoff_'+number+'_' + date + '.docx')
+        wb.save(r'EDI\writeoff_'+number+'_' + date + '.xlsx')
+        return r'EDI\writeoff_'+number+'_' + date + '.docx', r'EDI\writeoff_'\
+               + number + '_' + date + '.xlsx'
+
+    # накладная
+    elif doctype == 'waybill':
+        wb = load_workbook(rf'templates\attachment_template.xlsx')
+        ws = wb.active
+        doc = DocxTemplate(rf'templates\waybill_template.docx')
+        ws['B2'] = f'Накладной №' + str(number)
+        goods_list = ''
+        total = 0
+        for key, value in goods.items():
+            a = [key]
+            a.extend(list(value.values()))
+            ws.append(a)
+            total += value['amount'] * value['price']
+            goods_list += f"{key} x {value['amount']} "
+        context = {'number': number,
+                   'contract_num': contract_num,
+                   'date': date,
+                   'reciever': reciever,
+                   'driver': driver,
+                   'sum': str(total) + ' руб',
+                   'sender': sender,
+                   'goods': goods_list,
+                   'proxy': proxy}
+        doc.render(context)
+        doc.save(r'EDI\waybill_' + number + '_' + date + '.docx')
+        wb.save(r'EDI\waybill_' + number + '_' + date + '.xlsx')
+        return r'EDI\waybill_' + number + '_' + date + '.docx', r'EDI\writeoff_' \
+               + number + '_' + date + '.xlsx'
+
+
+# edi_export(date='12.12.2023',
+#            reciever='Андрей',
+#            sender='Костя',
+#            goods={'готовый проект': {'amount': 1,
+#                                      'price': 1000,
+#                                      'mass': 'very heavy'
+#                                      }})
+
+# edi_export(doctype='passport',
+#            pass_num=1707,
+#            date='12.13.2023',
+#            pass_date='11.13.2023',
+#            pass_exp_date='не ограничен',
+#            goods={'готовый проект': {'душность': 100,
+#                                      'стабильность': 0.1,
+#                                      'масса': 'very heavy',
+#                                      'exp_date': 'не ограничен'
+#                                      }})
+
+
+# edi_export(number='1',
+#            doctype='move_in',
+#            contract_num='12312',
+#            driver='Ярик',
+#            date='12.12.2023',
+#            reciever='ООО АЮК',
+#            sender='Бабушка',
+#            goods={'техзадание': {'amount': 1,
+#                                      'price': 1000,
+#                                      'mass': 'невыносимая'
+#                                  }})
+
+# edi_export(number='1',
+#            doctype='move_out',
+#            contract_num='12312',
+#            driver='Ярик',
+#            date='12.12.2023',
+#            reciever='Бабушка',
+#            sender='ООО АЮК',
+#            goods={'готовый проект': {'amount': 1,
+#                                      'price': 1000,
+#                                      'mass': '28 тысяч коммитов! ты дейстовал наверняка'
+#                                  }})
+
+# edi_export(number='1',
+#            doctype='write-off',
+#            date='12.12.2023',
+#            sender='ООО АЮК',
+#            reason='перегорели',
+#            goods={'человеческие мозги': {'amount': 3,
+#                                      'price': 1000,
+#                                      'mass': '1 кг'
+#                                  }})
+
+# edi_export(number='1',
+#            doctype='write-off',
+#            date='12.12.2023',
+#            sender='ООО АЮК',
+#            reason='перегорели',
+#            goods={'человеческие мозги': {'amount': 3,
+#                                      'price': 1000,
+#                                      'mass': '1 кг'
+#                                  }})
+
+
+# edi_export(number='1',
+#            doctype='waybill',
+#            contract_num='12312',
+#            driver='Ярик',
+#            date='12.12.2023',
+#            reciever='Бабушка',
+#            sender='ООО АЮК',
+#            goods={'готовый проект': {'amount': 1,
+#                                      'price': 1000,
+#                                      'mass': '28 тысяч коммитов! ты дейстовал наверняка'
+#                                      }})
+
+
+
+
+
+
