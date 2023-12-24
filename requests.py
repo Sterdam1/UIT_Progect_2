378be941379e5f9ef56caa5ba470503e785484d8
Andrew <bezb1112@yandex.ru>
2023-12-24 14:06:02 +0300

    all basic functions added - getter setter and deleter.

--- requests.py
+++ requests.py
@@ -21,7 +21,7 @@ class Double_Dragon:
 
     def get_table_names(self) -> list:
         """
-        gets all table names as strings.
+        gets all table names as strings. except temporal ones.
 
         :return: table names
         :rtype: list
@@ -29,8 +29,7 @@ class Double_Dragon:
         with self.con as con:
             names = con.execute("""SELECT name FROM sqlite_master  
           WHERE type='table'; """)
-            names = [i[0] for i in names.fetchall() if i[0] != 'sqlite_sequence']
-        return names
+        return [i[0] for i in names.fetchall() if i[0] != 'sqlite_sequence']
 
     def get_table_by_name(self, name: str) -> list:
         """
@@ -41,10 +40,64 @@ class Double_Dragon:
         :rtype: list
         """
         with self.con as con:
-            goods = con.execute(f""" select * from '{name}' """).fetchall()
-        return goods
+            return con.execute(f""" select * from '{name}' """).fetchall()
 
+    def get_items(self, table_name: str, values, by='id') -> list:
+        """
+        returns list of items searched by any value.
 
+        :param table_name: table name as string, as example 'Goods'
+        :param values: tuple (if tuple - at least 2 items) i.e. ('Шоколад','Книга') or string of text i.e. 'Шоколад'
+        :param by: string of text by which you filter table content
+        :return: list of matches
+        :rtype: list
+        """
+        with self.con as con:
+            if type(values) == str:
+                values = "'" + values + "'"
+                return con.execute(f""" select * from '{table_name}' where {by} = {values} """).fetchall()
+            return con.execute(f""" select * from '{table_name}' where {by} in {values} """).fetchall()
+
+    def del_table_content_by_ids(self, name: str, ids: list):
+        """
+        delete strings from any table. required name of table and list of ids
+
+        :param name: table name as string, as example 'Goods'
+        :param ids: list of id, as example [1, 2, 3] or [4]
+        """
+        print(f'''DELETE FROM '{name}'
+                            WHERE id in {ids}''')
+        with self.con as con:
+            if len(ids) > 1:
+                ids = tuple(ids)
+                con.execute(f'''DELETE FROM '{name}' WHERE id in {ids}''')
+            else:
+                ids = ids[0]
+                con.execute(f'''DELETE FROM '{name}' WHERE id in ({ids})''')
+
+    def insert(self, name: str, values: tuple):
+        """
+        insert values into table. required name of table and tuple of values. id is not required
+
+        :param name: table name as string, as example 'Goods'
+        :param values: tuple of values i.e. ('Книга', 350.0, 1, 1234, 0.5, 'Book_1.json', 1, 0, '2024-06-30', 50, 20, 0))
+        """
+        with self.con as con:
+            result = con.execute(f'''select * from pragma_table_info('{name}')''').fetchall()
+            listy = tuple([i[1] for i in result if i[1] != 'id'])
+            con.execute(f'''insert into '{name}' {listy} values {values}''')
+
+    # the same as it ever was
+    def update_cell(self, table: str, id: int, param: str, value: any):
+        """
+        Sets parameter(param) of item(by id) in table(table) to (value)
+        """
+        if type(value) == str:
+            value = "'" + value + "'"
+        with self.con as con:
+            con.execute(f'''UPDATE {table}
+                            SET '{param}' = {value}
+                            WHERE id = {id} ''')
 
 
 db = Double_Dragon()
@@ -78,4 +131,5 @@ db = Double_Dragon()
         паспорта
         
         Масса
+        select * from pragma_table_info('tblName')
 """
