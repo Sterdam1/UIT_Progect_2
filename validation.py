bc4568a05fe964642c9bc6322301d2869ea3e5ba
Sterdam <novkos2306@gmail.com>
2024-01-08 13:32:31 +0300

    add(patterns to validate): some new columns are validated

--- validation.py
+++ validation.py
@@ -6,11 +6,18 @@ class Valid:
         pass
 
     def isLike(self, text, name):
-        if name == 'цена товара':
-            return True if re.fullmatch(pattern=r'\d+\.\d', string=text) else False
+        if name.lower() in ['цена товара', "масса"]:
+            return True if re.fullmatch(pattern=r'^(?!0\d)(\d+)(\.\d*[1-9])?$', string=text) else False
+        elif name.lower() == 'свойства в формате json':
+            return True if re.fullmatch(pattern=r'.+\.json$', string=text) else False
+        elif name.lower() == 'дата истечения срока годности':
+            return True if re.fullmatch(pattern=r'^\d{4}-\d{2}-\d{2}$', string=text) else False
+        elif name.lower() == 'артикул':
+            return True if re.fullmatch(pattern=r'^\d{4}$', string=text) else False
         else:
             return None
         
     
 
 Validator = Valid()
+# print(Validator.isLike('5.10' ,'цена товара'))
